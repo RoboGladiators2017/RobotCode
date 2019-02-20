bf2f54837ad13394f96cf468176b053171c75b9b
Lidea Shahidi <lideashahidi@gmail.com>
2019-02-19 19:18:18 -0500

    Using Spark motor controllers

--- Desktop/sparkplusmotor/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ Desktop/sparkplusmotor/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -31,44 +31,41 @@ public class Drivetrain extends Subsystem {
   DifferentialDrive differentialDrive = null;
 
   public Drivetrain() {
-    // assign values to Spark variables
-    leftFrontPWMVICTORSPX = new PWMVictorSPX(RobotMap.DRIVETRAIN_LEFT_FRONT_PWMVICTORSPX); // port numbers defined in RobotMap
-    //leftFrontSpark = new Spark(RobotMap.DRIVETRAIN_LEFT_BACK_SPARK);
-    rightFrontPWMVICTORSPX = new PWMVictorSPX(RobotMap.DRIVETRAIN_RIGHT_FRONT_PWMVICTORSPX); 
-    //rightBackSpark = new Spark(RobotMap.DRIVETRAIN_RIGHT_BACK_SPARK); 
-    
-    // SpeedControllerGroup leftMotor = new SpeedControllerGroup(leftFrontSpark,leftBackSpark);
-    // SpeedControllerGroup rightMotor = new SpeedControllerGroup(rightFrontSpark,rightBackSpark);
+    // use following lines for new robot:
+    //leftFrontPWMVICTORSPX = new PWMVictorSPX(RobotMap.DRIVETRAIN_LEFT_FRONT_PWMVICTORSPX); // port numbers defined in RobotMap
+    //rightFrontPWMVICTORSPX = new PWMVictorSPX(RobotMap.DRIVETRAIN_RIGHT_FRONT_PWMVICTORSPX); 
 
-    //leftBackSpark.follow(leftFrontSpark);
-    //rightBackSpark.follow(rightFrontSpark);
+    // use following lines for old robot:
+    leftFrontPWMVICTORSPX = new Spark(RobotMap.DRIVETRAIN_LEFT_FRONT_PWMVICTORSPX);
+    rightFrontPWMVICTORSPX = new Spark(RobotMap.DRIVETRAIN_RIGHT_FRONT_PWMVICTORSPX); 
 
     differentialDrive = new DifferentialDrive(leftFrontPWMVICTORSPX, rightFrontPWMVICTORSPX);
   }
 
   public void arcadeDrive(double moveSpeed, double rotateSpeed) {
+
+    System.out.println("Before: " + rotateSpeed);
     // square the values of moveSpeed and rotateSpeed to dampen
     // keep negative values negative
     if(moveSpeed < 0) {
-      moveSpeed = - moveSpeed * moveSpeed;
+      moveSpeed = - (moveSpeed * moveSpeed);
     }
     else {
       moveSpeed = moveSpeed * moveSpeed;
     }
 
     if(rotateSpeed < 0) {
-      rotateSpeed = - rotateSpeed * rotateSpeed;
+      rotateSpeed = - (rotateSpeed * rotateSpeed);
     }
     else {
       rotateSpeed = rotateSpeed * rotateSpeed;
     }
-
+    System.out.println("After: " + rotateSpeed);
     differentialDrive.arcadeDrive(moveSpeed, rotateSpeed);
   }
 
   @Override
   public void initDefaultCommand() { 
-    System.out.println("jjkj");
     setDefaultCommand(new DriveArcade());
 
     // runs automatically everytime the subsystem is called
